645045595ca0048c2833ca46a572765bc9f67438
Mars_i <dwellerofuniverse@gmail.com>
2018-06-23 01:20:37 +0800

    更新bootstrap-vue包

--- pages/register/index.vue
+++ pages/register/index.vue
@@ -1,29 +1,26 @@
 <template>
   <b-container class="register-container">
-    <b-form class="register-form" @submit.prevent="register" v-if="!$store.state.authUser">
+    <b-form class="register-form" v-if="!$store.state.authUser" @submit.prevent="register">
       <h4>注册</h4>
-      <b-form-group label="手机号" label-for="phoneInput">
-        <b-form-input id="phoneInput" type="text"  v-model="form.cellphone" require></b-form-input>
+      <b-form-group label="手机号" label-for="phoneInput" id="phoneInputGroup">
+        <b-form-input id="phoneInput" type="text"  v-model="form.cellphone" required></b-form-input>
         <small class="text-danger">{{error.phone}}</small>
       </b-form-group>
-      <b-form-group label="验证码" label-for="codeInput" class="clearfix">
-        <b-form-input id="codeInput" type="text" v-model="form.code" v-on:input="check_code" require></b-form-input>
+      <b-form-group label="验证码" label-for="codeInput" class="clearfix" id="codeInputGroup">
+        <b-form-input id="codeInput" type="text" v-model="form.code" required></b-form-input>
         <small class="text-danger float-left code-error">{{error.code}}</small>
-        <b-button class="float-right code-button" v-on:click="send_sms">发送短信验证码</b-button>
+        <b-button class="float-right code-button" @click="send_sms" type="button">发送短信验证码</b-button>
       </b-form-group>
-      <b-form-group label="密码" label-for="passInput1">
-        <b-form-input id="passInput1" type="password" v-model="form.password1" v-on:input="check_pass1" require></b-form-input>
+      <b-form-group label="密码" label-for="passInput1" id="passInput1Group">
+        <b-form-input id="passInput1" type="password" v-model="form.password1" v-on:input="check_pass1" required></b-form-input>
         <small class="text-danger code-error">{{error.password1}}</small>
       </b-form-group>
-      <b-form-group label="重复密码" label-for="passInput2">
-        <b-form-input id="passInput2" type="password" v-model="form.password2" v-on:input="check_pass2" require></b-form-input>
-        <small class="text-danger code-error">{{error.password2}}</small>
+      <b-form-group label="重复密码" label-for="passInput2" id="passInput2Group">
+        <b-form-input id="passInput2" type="password" v-model="form.password2" v-on:input="check_pass2" required></b-form-input>
+        <small class="text-danger">{{error.password2}}</small>
       </b-form-group>
-      <b-form-group>
-        <div class="form-check">
-          <input type="checkbox" :checked="form.checked">
-          同意<a href="#">《STAR MOVIE注册协议》</a>
-        </div>
+      <b-form-group id="checkInputGroup4">
+        <b-form-checkbox id="checkInput" :checked="form.checked" required>同意<a href="#">《STAR MOVIE注册协议》</a></b-form-checkbox>
       </b-form-group>
       <b-button id="submit-button" type="submit">注册</b-button>
       <small class="text-danger">{{curError}}</small>
@@ -47,14 +44,14 @@
           cellphone: '',
           code: '',
           password1: '',
-          password2: '',
-          checked: false
+          password2: ''
         },
         error: {
           phone: '',
           code: '',
           password1: '',
-          password2: ''
+          password2: '',
+          checked: ''
         },
         curCode : "",
         curPhone: "",
@@ -75,16 +72,10 @@
       check_pass2() {
         if(this.form.password1 !== this.form.password2){
           this.error.password2 ="两次密码输入不同"
-        } else {
+        } else if(this.form.password2==""){
+          this.error.password2 = "请输入重复密码"
+        }else {
           this.error.password2 = ""
-        }
-      },
-
-      check_code() {
-        if(this.form.code!==this.curCode) {
-          this.error.code = "输入验证码错误"
-        } else {
-          this.error.code = ""
         }
       },
 
@@ -98,32 +89,41 @@
           return
         } else {
           this.error.phone = "";
-          return;
         }
-        this.curPhone = this.form.cellphone
-        this.curCode = ""
+        this.curPhone = this.form.cellphone;
+        this.curCode = "";
         for(let i=0;i<4;i++){
           this.curCode+=Math.floor(Math.random()*10)
         }
         try {
-          let {data} = await axios.post('/api/sms', {code: this.curCode, cellphone: this.form.cellphone});
+          await axios.post('/api/sms', {code: this.curCode, cellphone: this.form.cellphone});
         } catch (error) {
-          if (error.response && error.response.status === 401) {
-            throw new Error('发送验证码失败')
+            this.error.code= '发送验证码失败'
           }
-          throw error
-        }
       },
 
       async register() {
-        if(this.form.cellphone!==this.curPhone){
-          this.error.phone = "手机号码变更"
+        this.form.code!==this.curCode ? this.error.code = "输入验证码错误": this.error.code = "";
+
+        this.form.cellphone!==this.curPhone? this.error.phone = "手机号码变更":this.error.phone = "";
+
+        if(!this.error.phone&&!this.error.checked&&!this.error.password2&&!this.error.password1&&!this.error.code) {
+            alert("注册成功")
+            try {
+              await this.$store.dispatch('login', {
+                email: "137111111111",
+                password: "admin"
+              })
+              this.form.email = ''
+              this.form.password = ''
+              this.form.error = null
+              let path = this.$route.query.path || "/"
+              this.$router.replace(path)
+            } catch (e) {
+              this.form.error = e.message
+            }
         } else {
-          this.error.phone = ""
-        }
-        if(!error.phone || !error.code || !error.password1 || !error.password2 || !this.form.checked) {
-          this.curError = "输入出错，请修改"
-          return;
+          alert("注册失败")
         }
       }
     }
