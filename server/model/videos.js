2ea002c5bd3e7c343a1d58aa570355a45f65f4bb
Mars_i <dwellerofuniverse@gmail.com>
2018-06-25 12:47:35 +0800

    修改视频，资讯modelSchame

--- server/model/videos.js
+++ server/model/videos.js
@@ -8,8 +8,7 @@ const videoSchema = new Schema({
   id: String,
   title: String,  //视频标题
   abstract: String,//视频摘要
-  author: String, //视频作者
-  img_url: String, //视频封面图
+  cover_img_src: String, //视频封面图
   url: String, //视频链接  ---- 这里考虑是要直接连接到新闻还是生成新闻详情页面？
   date: String, //视频发布时间
   view_count: String, //视频播放人数
