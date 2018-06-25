9d500a2a77063dfcf0627a40470365ef451fc402
Mars_i <dwellerofuniverse@gmail.com>
2018-06-25 12:49:06 +0800

    修改modelSchame

--- server/model/news.js
+++ server/model/news.js
@@ -7,9 +7,10 @@ const newsSchema = new Schema({
   id: String,
   title: String,  //新闻标题
   abstract: String,//新闻摘要
-  author: String, //新闻作者
-  img_url: String, //新闻封面图
-  url: String, //新闻链接  ---- 这里考虑是要直接连接到新闻还是生成新闻详情页面？
+  article: String, //新闻作者
+  cover_img_src: String, //新闻封面图
+  content_img_src: Array,
+  url: String, //新闻链接
   date: String, //新闻发布时间
   view_count: String, //新闻浏览人数
 })
