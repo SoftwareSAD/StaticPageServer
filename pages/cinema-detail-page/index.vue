090eaf76f229fa4389545d566afa6bb0b3ad9067
hanxu1997 <807174205@qq.com>
2018-06-29 16:08:58 +0800

    修改cinema-detail-page背景

--- pages/cinema-detail-page/index.vue
+++ pages/cinema-detail-page/index.vue
@@ -308,6 +308,16 @@ export default {
 </script>
 
 <style>
+
+body, html {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+  font-size: 14px;
+  background-image: none;
+}
+
 .banner {
     background-color: rgba(153,170,200,0.9);
     width: 100%;
