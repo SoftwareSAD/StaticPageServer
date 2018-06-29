5828b1e6512f3d3eb60fedea9312f7d6f73dad3d
heygrandpa <1003099120@qq.com>
2018-06-30 01:18:17 +0800

    修改api：按照全部条件搜索电影院

--- server/api/cinema.js
+++ server/api/cinema.js
@@ -89,6 +89,54 @@ router.get('/getCinemaByName', async (req, res, next) => {
   return _dbSuccess(res, '获取' + cinemaName + '数据成功', findCinemas[0])
 });
 
-
+/**
+ * @desc 按照品牌及行政区获取影院数据
+ * @param district：行政区
+             brand: 品牌
+       currentpage: 当前第几页
+ * @return 影院信息
+ * */
+router.get('/getCinemaByAll', async (req, res, next) => {
+	let brand = req.query.brand
+	let district = req.query.district
+	let page = req.query.currentPage
+	let page_length = 10;   // 每一页有10个电影院
+	let count = (page - 1) * page_length
+	console.log('########按照全部搜索：获取10个电影院#########');
+	if (brand == '全部' && district == '全部') {   // 按照电影院名进行排序
+		let cinemaArr = await CinemaModel.find({}).limit(page_length).skip(count).sort({cinema_name: -1}).exec()
+		let findCinemas = []
+		for (let item of cinemaArr) {
+			let ob = JSON.parse(JSON.stringify(item))
+			findCinemas.push(ob)
+		}
+		return _dbSuccess(res, '获取电影院成功', findCinemas)
+	} else if (district == '全部') { // 只通过品牌查询
+		// 在电影院的名字中模糊搜索
+		let cinemaArr = await CinemaModel.find({cinema_name: {$regex: brand}}).limit(page_length).skip(count).sort({cinema_name: -1}).exec()
+		let findCinemas = []
+		for (let item of cinemaArr) {
+			let ob = JSON.parse(JSON.stringify(item))
+			findCinemas.push(ob)
+		}
+		return _dbSuccess(res, '获取电影院成功', findCinemas)
+	} else if (brand == '全部') {    // 只通过行政区查询
+		let cinemaArr = await CinemaModel.find({district: district}).limit(page_length).skip(count).exec()
+		let findCinemas = []
+		for (let item of cinemaArr) {
+			let ob = JSON.parse(JSON.stringify(item))
+			findCinemas.push(ob)
+		}
+		return _dbSuccess(res, '获取电影院成功', findCinemas)
+	} else {
+		let cinemaArr = await CinemaModel.find({cinema_name: {$regex: brand}}, {district: key}).limit(page_length).skip(count).exec()
+		let findCinemas = []
+		for (let item of cinemaArr) {
+			let ob = JSON.parse(JSON.stringify(item))
+			findCinemas.push(ob)
+		}
+		return _dbSuccess(res, '获取电影院成功', findCinemas)
+	}
+});
 
 export default router
