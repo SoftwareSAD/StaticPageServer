6ec7bac16c8395546745cfdc2a55921fd6b2a70a
heygrandpa <1003099120@qq.com>
2018-06-30 03:34:20 +0800

    add api of movie: sort and search

--- server/api/movie.js
+++ server/api/movie.js
@@ -167,6 +167,30 @@ router.get('/getMoviesByCountry', async (req, res, next) => {
       findMovies.push(ob)
     }
     return _dbSuccess(res, '获取电影成功', findMovies)
+  } else if (key == '日韩') {
+    let MoviesArr = await MovieModel.find({country: {$regex: $or:['日本', '韩国']}}).limit(page_length).skip(count).exec();
+    let findMovies = [];
+    for (let item of MoviesArr) {
+      let ob = JSON.parse(JSON.stringify(item));
+      findMovies.push(ob)
+    }
+    return _dbSuccess(res, '获取电影成功', findMovies)
+  } else if (key == '欧洲') {
+    let MoviesArr = await MovieModel.find({country: {$regex: $or:['法国', '意大利', '西班牙']}}).limit(page_length).skip(count).exec();
+    let findMovies = [];
+    for (let item of MoviesArr) {
+      let ob = JSON.parse(JSON.stringify(item));
+      findMovies.push(ob)
+    }
+    return _dbSuccess(res, '获取电影成功', findMovies)
+  } else if (key == '其他') {
+    let MoviesArr = await MovieModel.find({country: {$regex: $or:['泰国', '俄罗斯', '澳大利亚', '伊朗']}}).limit(page_length).skip(count).exec();
+    let findMovies = [];
+    for (let item of MoviesArr) {
+      let ob = JSON.parse(JSON.stringify(item));
+      findMovies.push(ob)
+    }
+    return _dbSuccess(res, '获取电影成功', findMovies)
   } else {
     let MoviesArr = await MovieModel.find({country: {$regex: key}}).limit(page_length).skip(count).exec();
     let findMovies = [];
@@ -178,8 +202,6 @@ router.get('/getMoviesByCountry', async (req, res, next) => {
   }
 });
 
-
-// TODO：此功能还未完善
 /**
  * @desc 根据时间查询电影
  * @param   online_time: 上映时间
@@ -210,20 +232,208 @@ router.get('/getMoviesByDate', async (req, res, next) => {
     }
     return _dbSuccess(res, '获取电影成功', findMovies)
   } else if (key == '2011-2017') {
-    
+    for (var year = 2011; year <= 2017; year++) {
+      let MoviesArr = await MovieModel.find({online_time: {$regex: String(year)}}).limit(page_length).skip(count).exec();
+      let findMovies = [];
+      for (let item of MoviesArr) {
+        let ob = JSON.parse(JSON.stringify(item));
+        findMovies.push(ob)
+      }
+      return _dbSuccess(res, '获取电影成功', findMovies)
+    }
   } else if (key == '2000-2010') {
-
+    for (var year = 2000; year <= 2010; year++) {
+      let MoviesArr = await MovieModel.find({online_time: {$regex: String(year)}}).limit(page_length).skip(count).exec();
+      let findMovies = [];
+      for (let item of MoviesArr) {
+        let ob = JSON.parse(JSON.stringify(item));
+        findMovies.push(ob)
+      }
+      return _dbSuccess(res, '获取电影成功', findMovies)
+    }
   } else if (key == '90年代') {
-
+    for (var year = 1990; year < 2000; year++) {
+      let MoviesArr = await MovieModel.find({online_time: {$regex: String(year)}}).limit(page_length).skip(count).exec();
+      let findMovies = [];
+      for (let item of MoviesArr) {
+        let ob = JSON.parse(JSON.stringify(item));
+        findMovies.push(ob)
+      }
+      return _dbSuccess(res, '获取电影成功', findMovies)
+    }
   } else if (key == '80年代') {
-
+    for (var year = 1980; year < 1990; year++) {
+      let MoviesArr = await MovieModel.find({online_time: {$regex: String(year)}}).limit(page_length).skip(count).exec();
+      let findMovies = [];
+      for (let item of MoviesArr) {
+        let ob = JSON.parse(JSON.stringify(item));
+        findMovies.push(ob)
+      }
+      return _dbSuccess(res, '获取电影成功', findMovies)
+    }
   } else if (key == '70年代') {
-
+    for (var year = 1970; year < 1980; year++) {
+      let MoviesArr = await MovieModel.find({online_time: {$regex: String(year)}}).limit(page_length).skip(count).exec();
+      let findMovies = [];
+      for (let item of MoviesArr) {
+        let ob = JSON.parse(JSON.stringify(item));
+        findMovies.push(ob)
+      }
+      return _dbSuccess(res, '获取电影成功', findMovies)
+    }
   } else if (key == '更早') {
-
+    for (var year = 1900; year < 1970; year++) {
+      let MoviesArr = await MovieModel.find({online_time: {$regex: String(year)}}).limit(page_length).skip(count).exec();
+      let findMovies = [];
+      for (let item of MoviesArr) {
+        let ob = JSON.parse(JSON.stringify(item));
+        findMovies.push(ob)
+      }
+      return _dbSuccess(res, '获取电影成功', findMovies)
+    }
   }
-  
+});
 
+/**
+ * @desc 按照热度对电影排序：只对2018年的电影排序，热度用票房衡量
+ * @param currentPage: 当前是第几页
+ * @return 返回30项
+ * */
+
+router.get('/getHotSortedMovies', async (req, res, next) => {
+  console.log('########按照热度对电影排序#########');
+  let page_length = 30;   // 获取30个
+  let page = req.query.currentPage
+  let count = (page - 1) * page_length
+  let MoviesArr = await MovieModel.find({online_time: {$regex: '2018'}}).exec();
+  let findMovies = [];
+  let YiArr = []    // 单位：亿
+  let WanArr = []   // 单位：万
+  for (let item of MoviesArr) {
+    let movie_price = item.movie_total_price
+    if (movie_price.indexOf('亿') != -1) {
+      let YiStr = item.movie_total_price.split('亿')[0]
+      let YiNum = Number(YiStr)
+      YiArr.push(YiNum)
+    } else if (movie_price.indexOf('万') != -1) {
+      let WanStr = item.movie_total_price.split('万')[0]
+      let WanNum = Number(WanStr)
+      WanArr.push(WanNum)
+    }
+  }
+  function compare(value1, value2) {
+    return value2 - value1
+  }
+  YiArr.sort(compare)
+  WanArr.sort(compare)
+  let storeMovie = []
+  for (var i = 0; i < (YiArr.length + WanArr.length); i++) {
+    if (i < YiArr.length) {
+      let price = String(YiArr[i]) + '亿'
+      let Arr = await MovieModel.find({movie_total_price: price}).exec();
+      for (let item of Arr) {
+        let ob = JSON.parse(JSON.stringify(item));
+        storeMovie.push(ob)
+      }
+    } else {
+      let price = String(WanArr[i - YiArr.length]) + '万'
+      let Arr = await MovieModel.find({movie_total_price: price}).exec();
+      for (let item of Arr) {
+        let ob = JSON.parse(JSON.stringify(item));
+        storeMovie.push(ob)
+      }
+    }
+  }
+  for (var i = count; i < count + page_length; i++) {
+    findMovies.push(storeMovie[i])
+  }
+  return _dbSuccess(res, '获取热映电影成功', findMovies)
+});
+
+/**
+ * @desc 按照评价对电影排序：只对2018年的电影排序，评分作为衡量标准
+ * @param currentPage: 当前是第几页
+ * @return 返回30项
+ * */
+
+router.get('/getEvalSortedMovies', async (req, res, next) => {
+  console.log('########按照评价对电影排序#########');
+  let page_length = 30;   // 获取30个
+  let page = req.query.currentPage
+  let count = (page - 1) * page_length
+  let MoviesArr = await MovieModel.find({online_time: {$regex: '2018'}}).exec();
+  let starArr = []
+  let findMovies = []
+  for (let item of MoviesArr) {
+    let movie_star = item.movie_star
+    if (movie_star.indexOf('.') != -1) {
+      let starNum = Number(movie_star)   // 把评分转为数字
+      starArr.push(starNum)
+    }
+  }
+  function compare(value1, value2) {
+    return value2 - value1
+  }
+  starArr.sort(compare)
+  for (var i = count; i < count + page_length; i++) {
+    let Arr = await MovieModel.find({movie_star: String(starArr[i])}).exec();
+    for (let item of Arr) {
+      let ob = JSON.parse(JSON.stringify(item));
+      findMovies.push(ob)
+    }
+  }
+  return _dbSuccess(res, '获取高分电影成功', findMovies)
+});
+
+/**
+ * @desc 按照时间对电影排序：对所有电影进行排序
+ * @param currentPage: 当前是第几页
+ * @return 返回30项
+ * */
+
+router.get('/getOnlineSortedMovies', async (req, res, next) => {
+  console.log('########按照时间对电影排序#########');
+  let page_length = 30;   // 获取30个
+  let page = req.query.currentPage
+  let count = (page - 1) * page_length
+  let MoviesArr = await MovieModel.find({}).sort(online_time: -1).exec();
+  
+  return _dbSuccess(res, '获取时间排序电影成功', findMovies)
+});
+
+/**
+ * @desc 获取经典影片：对所有电影排序，评分作为衡量标准
+ * @param currentPage: 当前是第几页
+ * @return 返回30项
+ * */
+
+router.get('/getEvalSortedMovies', async (req, res, next) => {
+  console.log('########按照评价对电影排序#########');
+  let page_length = 30;   // 获取30个
+  let page = req.query.currentPage
+  let count = (page - 1) * page_length
+  let MoviesArr = await MovieModel.find({}).exec();
+  let starArr = []
+  let findMovies = []
+  for (let item of MoviesArr) {
+    let movie_star = item.movie_star
+    if (movie_star.indexOf('.') != -1) {
+      let starNum = Number(movie_star)   // 把评分转为数字
+      starArr.push(starNum)
+    }
+  }
+  function compare(value1, value2) {
+    return value2 - value1
+  }
+  starArr.sort(compare)
+  for (var i = count; i < count + page_length; i++) {
+    let Arr = await MovieModel.find({movie_star: String(starArr[i])}).exec();
+    for (let item of Arr) {
+      let ob = JSON.parse(JSON.stringify(item));
+      findMovies.push(ob)
+    }
+  }
+  return _dbSuccess(res, '获取经典高分电影成功', findMovies)
 });
 
 /**
